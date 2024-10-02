8bc667bf4293c0b7e92d39f8d0c3e1d558f1dc9b
tusharswami534 <tusharswami17@gmail.com>
2024-10-02 12:58:13 +0530

    add hover and extra space

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,65 +6,65 @@ const navmenu = () => {
   document.getElementById("nav-line").classList.toggle("after:rotate-90");
   document.getElementById("nav-line-out-side").classList.toggle("translate-x-6");
   document.getElementById("nav-line-out-side-1").classList.toggle("translate-x-6");
-  document.body.classList.toggle("overflow-hidden");}
+  document.body.classList.toggle("overflow-hidden");
+}
 
-  // First Slider 
-  $('.slider-box').slick({
-      infinite: true,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      variableWidth: true,
-      nextArrow: ".next",
+//  Extensive Exercise Library
+$('.features-slider').slick({
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  variableWidth: true,
+  nextArrow: ".next",
   prevArrow: ".prev",
-    });
+});
 
-  // Slider Footer
-  $(document).ready(function() {
-      $('.slider-lg-screen').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: false,
-        fade: true,
-        asNavFor: '.slider-sm-screen',
-        infinite: true,
-      });
+// Know More Slider
+$(document).ready(function () {
+  $('.know-more-big-screen').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.know-more-small-screen',
+    infinite: true,
+  });
 
-      $('.slider-sm-screen').slick({
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        asNavFor: '.slider-lg-screen',
-        focusOnSelect: true,
+  $('.know-more-small-screen').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    asNavFor: '.know-more-big-screen',
+    focusOnSelect: true,
+    infinite: true,
+    variableWidth: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
           infinite: true,
-          variableWidth: true, 
-          responsive: [
-            {
-              breakpoint: 1024,
-              settings: {
-                slidesToShow: 3,
-                slidesToScroll: 1,
-                infinite: true,
-                centerMode: true,
-                dots: false
-              }
-            },
-            {
-              breakpoint: 600,
-              settings: {
-                slidesToShow: 2,
-                centerMode: true,
-                slidesToScroll: 1
-              }
-            },
-            {
-              breakpoint: 480,
-              settings: {
-                slidesToShow: 1,
-                centerMode: true,
-                slidesToScroll: 1
-              }
-            }
-          ]
+          centerMode: true,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          centerMode: true,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          centerMode: true,
+          slidesToScroll: 1
+        }
+      }
+    ]
 
-      });
-    });
-  
+  });
+});
